a757023be400f680d88c4253905e77c4ac12f70e
unknown <sujoydaspc2023@gmail.com>
2025-01-12 16:01:53 +0600

    adding firebase and authorization

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,32 @@
 
+import { getAuth, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+
+  const [user, setUser] = useState(null)
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const loggedInUser = result.user;
+      setUser(loggedInUser)
+    })
+    .catch(error => {
+      console.log('error',error.message);
+    })
+  }
+  console.log("User",user);
+
   return (
     <div className="mx-20 min-h-[90vh] flex justify-center items-center">
-        <button className="bg-emerald-400 text-white px-4 py-2 rounded-lg hover:bg-emerald-300 active:scale-95 transition-all">Google Login</button>
+
+      <div>
+        <button onClick={handleGoogleSignIn} className="bg-emerald-400 text-white px-4 py-2 rounded-lg hover:bg-emerald-300 active:scale-95 transition-all">Google Login</button>
+      </div>
     </div>
   )
 }
--- src/__steps.js
+++ src/__steps.js
@@ -1,9 +1,41 @@
 /**
  * 1. visit: console.firebase.google.com
- * 2. create project (slip google analytics) 
+ * 
+ * 2. create project (skip google analytics) 
+ * 
  * 3. register app (create config)
+ * 
  * 4. install firebase: npm install firebase
+ * 
  * 5. add config file to : firebase.init.js
- * 6. DANGER: Do not publish or make firebase config to public by pushing those to github
- * 7: Visit: goto Docs > Build > Authentication > Web > Get Started
+ * 
+ * 6. export app from the firebase.init.js
+ * 
+ * 7. DANGER: Do not publish or make firebase config to public by pushing those to github
+ * 
+ * 8. Visit: goto Docs > Build > Authentication > Web > Get Started
+ * 
+ * 9. Login.jsx: import getAuth from firebase/auth
+ * 
+ * 10.create const auth = getAuth(app)
+ * 
+ * 11. import googleAuthProvider and create a new provider
+ * 
+ * 12. create a function whitch is use in onlcick for login button
+ * 
+ * 13. in the fuction use signInWithPopUp and pass auth and provider
+ * 
+ * 14. use promice like this
+ *  .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      console.log('error',error.message);
+    })
+
+ * 15. go to project > Build >  Authentication > Sign-In method and  activate sign-in method (google, facebook, github, etc.)
+ * 
+ * 16. 
+ * 
  */
